95c74c6b6e778a01fa42c4051c9e768bd17c79c8
*PrateekBanwari* <*prateekbanwari0@gmail.com*>
2026-09-05 22:08:41 +0530

    added dashboard backend

---
+++ module/github/lib/github.ts
@@ -0,0 +1,78 @@
+import { Octokit } from "octokit"
+import { auth } from "@/lib/auth"
+import prisma from "@/lib/db"
+import { headers } from "next/headers"
+
+// Getting the github access token
+
+export const getGithubToken = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  if (!session) {
+    throw new Error("Unauthorized")
+  }
+
+  const account = await prisma.account.findFirst({
+    where: {
+      userId: session.user.id,
+      providerId: "github",
+    },
+  })
+  if (!account) {
+    throw new Error("No github access token found")
+  }
+  return account.accessToken
+}
+
+export const fetchUserContribution = async (
+  token: string | null,
+  userName: string
+) => {
+  const octokit = new Octokit({ auth: token })
+
+  const query = `
+   query($userName:String){
+        user(login:$userName){
+            contributionCollection {
+                contributionCalender{
+                   totalContributions
+                   weeks{
+                       contributionDays{
+                           contributionCount
+                           data
+                           color
+                       }
+                   }
+                }
+                
+            }  
+        }
+   }
+   `
+    // interface contributiondata{
+    //     user:{
+    //         contributionCollection:{
+    //             contributionCalender:{
+    //                 totalContributions:number,
+    //                 weeks:{
+    //                     conrtibutionCount:number,
+    //                     data:string | Date,
+    //                     color:string
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+   try {
+    const response:any = await octokit.graphql(query,{
+        userName
+    })
+    return response.user.contributionCollection.contributionCalender
+
+   } catch (error) {
+    
+   }
+}
